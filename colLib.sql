f2895bae22b2c75dec126b04e3d56d1276124e31
SwabhimanP <swabhimanpatnaik@gmail.com>
2021-11-22 14:55:32 +0530

    Adding source code

---
+++ colLib.sql
@@ -0,0 +1,18 @@
+create schema if not exists `hibernate_one-to-one_uni`;
+use `hibernate_one-to-one_uni`;
+
+insert into book(name, author, category) values ('A Song of Ice and Fire: A Game of Thrones','George RR Martin', 'Fantasy');
+insert into book(name, author, category) values ('A Song of Ice and Fire: A Clash of Kings','George RR Martin', 'Fantasy');
+insert into book(name, author, category) values ('A Song of Ice and Fire: A Storm of Swords','George RR Martin', 'Fantasy');
+insert into book(name, author, category) values ('A Song of Ice and Fire: A Feast for Crows','George RR Martin', 'Fantasy');
+insert into book(name, author, category) values ('A Song of Ice and Fire: A Dance with Dragons','George RR Martin', 'Fantasy');
+insert into book(name, author, category) values ('A Song of Ice and Fire: The Winds of Winter','George RR Martin', 'Fantasy');
+insert into book(name, author, category) values ('A Song of Ice and Fire: A Dream of Springs','George RR Martin', 'Fantasy');
+
+insert into book(name, author, category) values ("Harry Potter and the Philosopher's Stone",'JK Rowling', 'Fantasy');
+insert into book(name, author, category) values ("Harry Potter and the Chamber of Secrets",'JK Rowling', 'Fantasy');
+insert into book(name, author, category) values ("Harry Potter and the Prisoner of Azkaban",'JK Rowling', 'Fantasy');
+insert into book(name, author, category) values ("Harry Potter and the Goblet of Fire",'JK Rowling', 'Fantasy');
+insert into book(name, author, category) values ("Harry Potter and the Order of Phoenix",'JK Rowling', 'Fantasy');
+insert into book(name, author, category) values ("Harry Potter and the Half-Blood Prince",'JK Rowling', 'Fantasy');
+insert into book(name, author, category) values ("Harry Potter and the Deathly Hallows",'JK Rowling', 'Fantasy');
